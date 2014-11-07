fd3461cc1c4cc446c72b01ffe9e352651827bf4d
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:43:15 +0100

    Cleanup tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,6 @@
-#This file is part sale_wishlist module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+# This file is part sale_wishlist module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 
 from .test_sale_wishlist import suite
 
