6530be3f12c87107b20833f0e836585ec66804ef
resteve <resteve@zikzakmedia.com>
2015-12-01 11:23:03 +0100

    Tests

--- tests/test_sale_wishlist.py
+++ tests/test_sale_wishlist.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
-# This file is part sale_wishlist module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the sale_wishlist module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class SaleWishlistTestCase(unittest.TestCase):
-    'Test Sale Wish List module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('sale_wishlist')
-
-    def test0005views(self):
-        'Test views'
-        test_view('sale_wishlist')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class SaleWishlistTestCase(ModuleTestCase):
+    'Test Sale Wishlist module'
+    module = 'sale_wishlist'
 
 
 def suite():
@@ -27,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         SaleWishlistTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
