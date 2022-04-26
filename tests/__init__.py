a40ac572ebac3c05e522cc2ad3631dfbbd9d1d6b
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-26 13:21:58 +0200

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++
@@ -1,7 +0,0 @@
-# This file is part sale_wishlist module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
-
-from .test_sale_wishlist import suite
-
-__all__ = ['suite']
